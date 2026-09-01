6966a78fec5c4c5db1616c7c328a000b338bdfa8
maezerus <maezzyloveria@gmail.com>
2026-09-01 15:52:03 +0800

    Update money input and denomination calculation
    Refactor code to accept user input for deposit amount and calculate denomination breakdown.

--- code_challenge2.py
+++ code_challenge2.py
@@ -1,7 +1,23 @@
-money = 8935
+money = input("Enter amount to deposit")
 
 print("=========== PH PESO DENOMINATION BREAK DOWN =============")
 print("CURRENT MONEY IS =====>", money)
+
+libo = money // 1000 #935 -> how to brimg down this value 8.935
+libo_sukli = money % 1000 # 935
+
+five_h = libo_sukli // 500
+five_sukli = money % 1000 # 935
+
+
+
+
+
+
+
+
+
+
 print("1000 - ", )
 print("500 - ",)
 print("200 - ",)
