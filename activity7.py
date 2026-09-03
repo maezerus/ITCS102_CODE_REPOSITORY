e62647c4b23cf9483a909c09ae93b9b6d1617362
maezerus <maezzyloveria@gmail.com>
2026-09-03 12:33:07 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,34 @@
+total = 0
+
+n1 = eval(input("Enter a number --> "))
+total *= n1
+
+n2 = eval(input("Enter a number --> "))
+total1 = total + n2
+
+n2 = eval(input("Enter a number --> "))
+total = total1 + n2
+
+n3 = eval(input("Enter a number --> "))
+total = total + n3
+
+n4 = eval(input("Enter a number --> "))
+total = total + n4
+
+n5 = eval(input("Enter a number --> "))
+total1 = total + n5
+
+n6 = eval(input("Enter a number --> "))
+total = total + n6
+
+n7 = eval(input("Enter a number --> "))
+total = total + n7
+
+n8 = eval(input("Enter a number --> "))
+total = total + n8
+
+n9 = eval(input("Enter a number --> "))
+total = total + n9
+
+
+print("The sum is ", total)
---
+++ activity8.py
@@ -0,0 +1,9 @@
+hobbies = " " #blank sting
+
+h = input("What are your hobbies? --> ")
+hobbies += h + ", "
+
+h = input("What else? --> ")
+hobbies += h +", "
+
+print("so my hobbies are", hobbies)
